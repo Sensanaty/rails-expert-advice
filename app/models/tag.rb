6f0ffbddc1d532e465eda8dfe14a82f61fb54f16
Sensanaty <salevic@lu-ka.me>
2021-04-19 23:21:28 +0200

    Add before_save lambda & custom validation for the Tag name
    - The lambda will just downcase the text
    - The validation checks whether the name follows the StackOverflow format for question tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,19 @@
 class Tag < ApplicationRecord
+  before_save -> (name) { name.downcase! }
   validates :name, presence: true, uniqueness: true
+  validate :name_format
 
   has_many :question_tags, dependent: :destroy
   has_many :questions, through: :question_tags
+
+  private
+
+  def name_format
+    # StackOverflow-like tag names
+    # Any combination of alphanumerical characters, case insensitive
+    # 0 or 1 of . , or _ followed by more alphanumericals
+    # A maximum of 3 periods, dashes or underscores allowed in 1 tag
+    format_regex = /\A[^\W_]+(?:[-_.][^\W_]+){0,3}\z/
+    errors.add(:name, "invalid format") unless name.match?(format_regex)
+  end
 end
