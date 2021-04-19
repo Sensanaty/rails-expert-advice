0bd464ea66ab2dda0ea525a48c5d0d51651d2fec
Sensanaty <salevic@lu-ka.me>
2021-04-19 21:00:55 +0200

    Create a basic Tag model with appropriate relations
    - Created a Tag model and a QuestionTag model
    - The QuestionTag model serves to index the relationship between the Tags and Questions
    
    Also added a migration to add a view counter to the Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ApplicationRecord
   validates :user_id, :title, :content, presence: true
 
   belongs_to :user
+  has_many :question_tags, dependent: :destroy
+  has_many :tags, through: :question_tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_153949) do
+ActiveRecord::Schema.define(version: 2021_04_19_184809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2021_04_19_153949) do
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
   end
 
+  create_table "question_tags", force: :cascade do |t|
+    t.bigint "question_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_question_tags_on_question_id"
+    t.index ["tag_id"], name: "index_question_tags_on_tag_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.string "content"
@@ -68,6 +77,13 @@ ActiveRecord::Schema.define(version: 2021_04_19_153949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name"
   end
 
   create_table "user_account_accesses", force: :cascade do |t|
@@ -91,6 +107,8 @@ ActiveRecord::Schema.define(version: 2021_04_19_153949) do
   add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
   add_foreign_key "oauth_access_grants", "users", column: "resource_owner_id"
   add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
+  add_foreign_key "question_tags", "questions"
+  add_foreign_key "question_tags", "tags"
   add_foreign_key "questions", "users"
   add_foreign_key "user_account_accesses", "accounts"
   add_foreign_key "user_account_accesses", "users"
