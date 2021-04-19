2247b12129d07f5bcd43d0ba4823bf810c4cdcfd
Sensanaty <salevic@lu-ka.me>
2021-04-19 23:21:20 +0200

    Run migration for question views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_184809) do
+ActiveRecord::Schema.define(version: 2021_04_19_185838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_184809) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
