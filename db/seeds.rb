bb14cd5d3e4ca1b8f9ca6e566f9aef561339eda1
Sensanaty <salevic@lu-ka.me>
2021-04-20 15:34:04 +0200

    Create basic seed for all currently existing models
    Creates 1 user, 1 Question and a Tag belonging to that question and prints those out to the console
    
    Signed-off-by: Sensanaty <salevic@lu-ka.me>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,33 @@
+require 'faker'
+
+puts "Destroying old DB entries..."
 Doorkeeper::Application.destroy_all
+User.destroy_all
+Question.destroy_all
+Tag.destroy_all
+QuestionTag.destroy_all
+puts "DB Emptied"
+
+puts "Creating Doorkeeper application"
 Doorkeeper::Application.create(name: "expert_advice_web", redirect_uri: "http://localhost:3000/")
+
+puts "Creating a user"
+puts "\nemail: user@email.com"
+puts "password: password"
+user = User.create(email: "user@email.com", password: "password")
+
+puts "\nCreating a question belonging to the user"
+question = Question.create(
+  title: "Question Title",
+  content: Faker::Lorem.sentence(word_count: 30, random_words_to_add: 15),
+  user: user
+)
+
+puts "Creating a tag belonging to the question"
+tag = question.tags.create(name: "question-tag")
+
+puts "\nUser: #{user.inspect}"
+puts "Question: #{question.inspect}"
+puts "Tag: #{tag.inspect}"
+
+puts "Seeding done!"
