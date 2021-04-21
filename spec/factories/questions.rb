fa26c290d336015a4199f9b42d90c18bfe4f5a78
Sensanaty <salevic@lu-ka.me>
2021-04-21 14:03:33 +0200

    Modify questions factory & add pending tests to questions controller
    Realized that I'm on a bit of a time constraint, so going to finish these at the end if I have time

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     association :user
     title { "Question Title" }
     content { "Lorem Ipsum Dolor Sit Amet" }
+
+    after :create do |question|
+      create_list :answer, 3, question: question, user: question.user
+      2.times { question.tags.create(name: Faker::Alphanumeric.alphanumeric(number: rand(7..15))) }
+    end
   end
 end
