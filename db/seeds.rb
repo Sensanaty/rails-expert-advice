d8761e2d373ca7cfb87883994e102a1499cf143e
Sensanaty <salevic@lu-ka.me>
2021-04-23 12:58:18 +0200

    Modify seed to create slightly better questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,42 +22,27 @@ AccountUser.new(user: user, account: account)
 user.save
 
 puts "\nCreating a question belonging to the user"
-question = Question.create(
-  title: Faker::Quote.famous_last_words,
+Question.create(
+  title: (Faker::Quote.famous_last_words).gsub(/\.\z/, "?"),
   content: Faker::Lorem.paragraphs(number: rand(15..25)).join("\n"),
   user: user
 )
 
 puts "Creating a tag belonging to the question"
-tag = question.tags.create(name: "question-tag")
-second_tag = question.tags.create(name: "second-tag")
+tag = Question.first.tags.create(name: "question-tag")
+second_tag = Question.first.tags.create(name: "second-tag")
 
 puts "Creating 20 questions with random content"
 
 20.times do
-  random_question = Question.create(
-    title: Faker::Quote.famous_last_words,
+  Question.create(
+    title: (Faker::Quote.famous_last_words).gsub!(/\.\z/, "?"),
     content: Faker::Lorem.paragraphs(number: rand(15..25)).join("\n"),
     user: user
-  )
-  random_question.tags.create(name: Faker::Alphanumeric.alphanumeric(number: rand(7..15)))
-  random_question.tags.create(name: "question-tag")
-
-  Answer.create(content: Faker::Quote.famous_last_words, user: user, question: random_question)
-end
-
-puts "Creating Answers for original question"
-
-20.times do
-  Answer.create(
-    content: Faker::Lorem.paragraph,
-    user: user,
-    question: question
   )
 end
 
 puts "\nUser: #{user.inspect}"
-puts "Question: #{question.inspect}"
 puts "Tag: #{tag.inspect}"
 puts "Second Tag: #{second_tag.inspect}"
 
